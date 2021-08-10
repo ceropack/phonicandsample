732fbfb15ac055da8bd6d90d11460f1e15d77b25
ceropack <ceropacking@gmail.com>
2021-08-10 08:43:57 +0000

    2nd commit

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // [START gae_node_request_example]
 const express = require('express');
 
+
+
+
 const app = express();
 const fs = require('fs');
 
@@ -8,12 +11,20 @@ const fs = require('fs');
 
 const phonic_o = fs.readFileSync('phonic_ex.json');
 const personObject = JSON.parse(phonic_o);
-
-
 app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
   res.render('server', {  name: "Phonic",  items: personObject  })
-                               });
+    });
+
+
+
+const phonic_osa = fs.readFileSync('phonic_sa.json');
+const personObjectsa = JSON.parse(phonic_osa);    
+app.get('/sa', function (req, res) {
+  res.render('server', {  name: "Phonic-Saira",  items: personObjectsa  })
+    });
+
+
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
